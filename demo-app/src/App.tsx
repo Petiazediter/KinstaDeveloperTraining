caf386053301e9f2b1b7c3e5b52e9f3a21599962
Peti <pverebes74@gmail.com>
2021-06-25 12:44:54 +0200

    Add basic routing for project.

--- demo-app/src/App.tsx
+++ demo-app/src/App.tsx
@@ -1,8 +1,17 @@
+import { DatePicker ,Layout, Menu} from "antd"
+import 'antd/dist/antd.css';
+import { Route,BrowserRouter } from "react-router-dom"
+
+const { Header } = Layout;
+const { SubMenu } = Menu; 
+
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+      <Route path="/" exact>
         <h1>Hello world!</h1>
-    </div>
+      </Route>
+    </BrowserRouter>
   );
 }
 
