0ad8eb27ec00bc6d91e561aafca3f7b50c6fdf1d
Peti <pverebes74@gmail.com>
2021-06-25 13:06:56 +0200

    Create navbar component.

--- demo-app/src/App.tsx
+++ demo-app/src/App.tsx
@@ -1,10 +1,12 @@
 import { DatePicker ,Layout, Menu} from "antd"
 import 'antd/dist/antd.css';
 import { Route,BrowserRouter } from "react-router-dom"
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Route path="/" exact>
         <h1>Hello world!</h1>
       </Route>
---
+++ demo-app/src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from "react";
+
+export function Navbar(): ReactElement {
+    return (
+        <h1>Hello navbar world!</h1>
+    )
+}
